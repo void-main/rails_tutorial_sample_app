3d11f1fd4aee60f3434de126e3fafe9e418e50b0
Void Main <voidmain1313113@gmail.com>
2012-09-18 10:33:55 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '548eb633ccb2ef0dea84980a75880f6942abaacd14c8a9106f1d74d596238cbbe9a588b7c93c0096ace07b71f87b1d597fe24fbc7eb09ebc72cf29c77c2484dd'
